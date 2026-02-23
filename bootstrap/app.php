20f8331257f9e6b61cbbd1c441ee22e30f2ff816
ayouberrak <ayouberrak30@gmail.com>
2026-02-23 23:52:05 +0000

    chore: update application bootstrap configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\Ban;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withMiddleware(function(Middleware $middleware):void{
         $middleware->alias([
-            'ban'=> \App\Http\Middleware\Ban::class 
+            'ban'=> Ban::class 
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
