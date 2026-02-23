226d1daa57c24aed826d84a03d6398c76dcbdf3b
ayouberrak <ayouberrak30@gmail.com>
2026-02-23 14:38:06 +0000

    feat: add middleware for admin access control

---
+++ app/Http/Middleware/Ban.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+
+class Ban
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if($request->user() && $request->user()->is_banned){
+            Auth::logout();
+            return redirect()->route('login')->with('error','tbanitttttttiiiiiii');
+        }
+        return $next($request);
+    }
+}
