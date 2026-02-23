cc08ec3fa915894ff0dc32920a5ba39679d51651
ayouberrak <ayouberrak30@gmail.com>
2026-02-23 17:06:50 +0000

    refactor: update Colocation model relationships

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Colocation extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'colocation_id',
         'name',
