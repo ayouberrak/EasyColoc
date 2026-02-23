b0df37a5c125f536d6bbee835c7cf7871dbeb28e
ayouberrak <ayouberrak30@gmail.com>
2026-02-23 14:38:05 +0000

    chore: update application bootstrap configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        ]);
+    })
+    ->withMiddleware(function(Middleware $middleware):void{
+        $middleware->alias([
+            'ban'=> \App\Http\Middleware\Ban::class 
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
