b611ee5d159e21e334459aeaf86718ca69883e84
ayouberrak <ayouberrak30@gmail.com>
2026-02-24 15:06:39 +0000

    feat: improve colocation controller logic

--- app/Http/Controllers/ColocationController.php
+++ app/Http/Controllers/ColocationController.php
@@ -6,6 +6,11 @@ use App\Models\Colocation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
+use App\Models\ColocationMember;
+use Illuminate\Support\Facades\Mail;
+use App\Models\User;
+use App\Models\Invitation;
+use App\Mail\InvitationMail;
 
 
 
@@ -26,7 +31,77 @@ class ColocationController extends Controller
             'user_id'=> Auth::user()->id
         ]);
 
+        $membre = ColocationMember::create([
+            'colocation_id' => $colo->id,
+            'user_id' => Auth::user()->id,
+            'role' => 'owner',
+            'status' => 'active'
+        ]);
+
 
         return redirect(route('owner.dashboard'));
+    }
+
+    public function invite(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'colocation_id' => ['required', 'exists:colocations,id']
+        ]);
+
+        $colo = Colocation::findOrFail($request->colocation_id);
+        
+        $user = User::where('email', $request->email)->first();
+        if ($user && $colo->members()->where('user_id', $user->id)->exists()) {
+            return back()->with('error', 'error.');
+        }
+
+        $invi = Invitation::create([
+            'email' => $request->email,
+            'colocation_id' => $colo->id,
+            'token' => Str::random(40),
+            'status' => 'pending'
+        ]);
+
+        Mail::to($request->email)->send(new InvitationMail($invi));
+
+        return back()->with('success', 'invitation envoyee');
+    }
+
+    public function showInvitation($token)
+    {
+        $invitation = Invitation::where('token', $token)->where('status', 'pending')->firstOrFail();
+        return view('invitations.show', compact('invitation'));
+    }
+
+    public function acceptInvitation($token)
+    {
+        $invi = Invitation::where('token', $token)
+                            ->where('status', 'pending')
+                            ->firstOrFail();
+        $user = Auth::user();
+
+        if ($user->email !== $invi->email) {
+            return back()->with('error', 'eror.');
+        }
+
+        ColocationMember::create([
+            'colocation_id' => $invi->colocation_id,
+            'user_id' => $user->id,
+            'role' => 'member',
+            'status' => 'active'
+        ]);
+
+        $invi->update(['status' => 'accepter']);
+
+        return redirect()->route('dashboard');
+    }
+
+    public function declineInvitation($token)
+    {
+        $invi = Invitation::where('token', $token)->where('status', 'pending')->firstOrFail();
+        $invi->update(['status' => 'anuller']);
+
+        return redirect()->route('home');
     }
 }
