81e0d9c1bf67f7ed7205840c02167ad251e8bd4c
ayouberrak <ayouberrak30@gmail.com>
2026-02-24 15:06:40 +0000

    refactor: improve Category model structure

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $table = 'categories';
     protected $fillable =[
-        'category_id',
         'name'
     ];
 
