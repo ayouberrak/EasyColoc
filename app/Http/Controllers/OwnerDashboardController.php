971adc53de8907fde16b4237368d00ba389971d6
ayouberrak <ayouberrak30@gmail.com>
2026-02-24 15:06:39 +0000

    feat: improve owner dashboard features

--- app/Http/Controllers/OwnerDashboardController.php
+++ app/Http/Controllers/OwnerDashboardController.php
@@ -5,25 +5,51 @@ namespace App\Http\Controllers;
 use App\Models\Colocation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Invitation;
+use App\Models\Expense;
+use App\Models\Payment;
 
 class OwnerDashboardController extends Controller
 {
-    public function index(Request $request)
-    {
-        $colocation = Colocation::where('user_id', Auth::id())->first();
-        
-        if (!$colocation) {
-            return redirect()->route('colocations.create');
-        }
+public function index(Request $request)
+{
+    $user = Auth::user();
+    $colocation = Colocation::where('user_id', $user->id)->first();
 
-        $tab = $request->get('tab', 'dashboard');
-        
-        // Ensure only valid tabs are processed (optional but good practice)
-        $validTabs = ['dashboard', 'members', 'expenses', 'payments', 'chat'];
-        if (!in_array($tab, $validTabs)) {
-            $tab = 'dashboard';
-        }
-
-        return view('colocations.owner', compact('colocation', 'tab'));
+    if (!$colocation) {
+        return redirect()->route('colocations.create');
     }
+
+    $tab = $request->tab ?? 'dashboard';
+
+    $lien = ['dashboard', 'members', 'payments'];
+
+    if (!in_array($tab, $lien)) {
+        $tab = 'dashboard';
+    }
+
+    $search = $request->search;
+
+    
+    $query = User::where('id', '!=', $user->id);
+
+    if ($search) {
+        $query->where('name', 'like', '%' . $search . '%');
+    }
+
+    $num = $query->get();
+
+    $pendingInvitations = Invitation::where('colocation_id', $colocation->id)
+        ->where('status', 'pending')
+        ->get();
+
+    return view('colocations.owner', [
+        'colocation' => $colocation,
+        'tab' => $tab,
+        'potentialMembers' => $num,
+        'search' => $search,
+        'pendingInvitations' => $pendingInvitations
+    ]);
+}
 }
