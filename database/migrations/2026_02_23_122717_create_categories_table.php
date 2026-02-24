97960b6779f6bc57402187edf927702273136431
ayouberrak <ayouberrak30@gmail.com>
2026-02-24 15:06:40 +0000

    fix: update categories migration

--- database/migrations/2026_02_23_122717_create_categories_table.php
+++ database/migrations/2026_02_23_122717_create_categories_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('category_id')->nullable();
             $table->string('name');
             $table->timestamps();
         });
