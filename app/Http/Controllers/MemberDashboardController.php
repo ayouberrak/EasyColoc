f1d73a1ca60ab831a0c457e8a2728de689e5145a
ayouberrak <ayouberrak30@gmail.com>
2026-02-24 15:06:39 +0000

    feat: improve member dashboard features

--- app/Http/Controllers/MemberDashboardController.php
+++ app/Http/Controllers/MemberDashboardController.php
@@ -6,23 +6,35 @@ use App\Models\Colocation;
 use App\Models\ColocationMember;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Category;
 
 class MemberDashboardController extends Controller
 {
+
+    public function getAllCategories(){
+        $categories = Category::all();
+        return $categories;
+    }
     public function index(Request $request)
     {
         $user = Auth::user();
-        $activeMember = ColocationMember::where('user_id', $user->id)
+        $member = ColocationMember::where('user_id', $user->id)
             ->whereNull('left_at')
             ->first();
 
-        if (!$activeMember) {
+        if (!$member) {
             return redirect()->route('colocations.create');
         }
 
-        $colocation = $activeMember->colocation;
+        $colocation = $member->colocation;
         $tab = $request->get('tab', 'dashboard');
+        
+        $lien = ['dashboard', 'members', 'expenses'];
+        if (!in_array($tab, $lien)) {
+            $tab = 'dashboard';
+        }
 
-        return view('colocations.member', compact('colocation', 'tab'));
+        $categories = $this->getAllCategories();
+        return view('colocations.member', compact('colocation', 'tab', 'categories'));
     }
 }
