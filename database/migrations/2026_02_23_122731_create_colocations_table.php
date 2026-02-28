d169e4e25a8e401a8b2cefea6956b825c9b02a08
ayouberrak <ayouberrak30@gmail.com>
2026-02-28 22:59:36 +0000

    fix: update colocations migration

--- database/migrations/2026_02_23_122731_create_colocations_table.php
+++ database/migrations/2026_02_23_122731_create_colocations_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->string('status')->default('active');
-            $table->string('token')->unique();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
